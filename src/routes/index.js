7a3b50c0e11bcf876bf21cc55b02db72fe1ceb64
Thiago Lins <taglins@gmail.com>
2021-09-30 17:34:24 -0300

    Alterando Estrutura do Arquivo Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-const authController = require("./controllers/Auth");
-const mainController = require("./controllers/Main");
+const authController = require("../controllers/Auth");
+const mainController = require("../controllers/Main");
 
 router.get("/", authController.showLogin);
 router.get("/login", authController.showLogin);
