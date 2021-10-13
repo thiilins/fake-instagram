acca8601736f5821fa437f3050c4b4c374eddcfb
Thiago Lins <taglins@gmail.com>
2021-10-13 18:58:44 -0300

    Corrigindo script SQL e Configurações sequelize

--- src/database/script.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE fake_instagram;
-
-CREATE TABLE fake_instagram.users (
-	id int(10) PRIMARY KEY auto_increment,
-	name varchar(200),
-	email varchar(200),
-	password varchar(200),
-	create_at TIMESTAMP  
-);
