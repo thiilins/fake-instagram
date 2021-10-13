2d9741015162bd3e3cd4465adba204583881967d
Thiago Lins <taglins@gmail.com>
2021-10-13 19:37:41 -0300

    Atualizações DB

--- src/database/fake_instagram.sql
+++ src/database/fake_instagram.sql
@@ -1,8 +1,6 @@
-
+DROP DATABASE IF EXISTS `fake_instagram`;
 CREATE DATABASE IF NOT EXISTS `fake_instagram` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `fake_instagram`;
-
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) DEFAULT NULL,
@@ -14,4 +12,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
+
